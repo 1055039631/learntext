bb33c999002ddb9b697c19948fd5f01a1dcb84c8
1055039631 <83872842+1055039631@users.noreply.github.com>
2021-05-09 12:11:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ sudo yum install zip unzip
 sudo wget https://raw.githubusercontent.com/1055039631/learntext/main/config.json
 #sudo wget https://raw.githubusercontent.com/1055039631/learntext/main/build.sh
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+#bash <(curl -L https://raw.githubusercontent.com/1055039631/learntext/main/install.sh)
 echo "copy config to v2ray"
 cp -rf  config.json /usr/local/etc/v2ray/
 echo "systemctl stop firewalld"
